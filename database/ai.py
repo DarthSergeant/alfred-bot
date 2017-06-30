9bdb4fe6f1a8d005ffe6da5f9f0019cc01e409bc
DarthSergeant <darthsergeant@gmail.com>
2017-06-30 04:42:36 -0400

    Update ai.py

--- database/ai.py
+++ database/ai.py
@@ -1,5 +1,11 @@
 import random
 from database.cat_facts import catfacts
+import os
+import sys
+import json
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from flask import Flask, request
 
 def create_response(sentence):
     msg = {}
