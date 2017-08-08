0589905253eaff31a1c5f55fdd6047c6f9d38353
DarthSergeant <darthsergeant@gmail.com>
2017-08-08 10:51:16 -0400

    Create office_hours.py

---
+++ bot_core/hrl/information/office_hours.py
@@ -0,0 +1,66 @@
+"""##########################
+# Meena Hours
+##########################"""
+###Monday
+meena_monday = ['''Monday:
+8-5''']
+
+###Tuesday
+meena_tuesday = ['''Tuesday:
+8-5''']
+
+###Wednesday
+meena_wednesday = ['''Wednesday:
+8-5''']
+
+###Thursday
+meena_thursday = ['''Thursday:
+8-5''']
+
+###Friday
+meena_friday = ['''Friday:
+8-5''']
+
+"""##########################
+# Gabe Hours
+##########################"""
+###Monday
+gabe_monday = ['''Monday:
+8-5''']
+
+###Tuesday
+gabe_tuesday = ['''Tuesday:
+8-5''']
+
+###Wednesday
+gabe_wednesday = ['''Wednesday:
+8-5''']
+
+###Thursday
+gabe_thursday = ['''Thursday:
+8-5''']
+
+###Friday
+gabe_friday = ['''Friday:
+8-5''']
+
+"""##########################
+# Sam Hours
+##########################"""
+sam_monday = ['''Monday:
+8-5''']
+
+sam_tuesday = ['''Tuesday:
+8-5''']
+
+sam_wednesday = ['''Wednesday:
+8-5''']
+
+sam_thursday = ['''Thursday:
+8-5''']
+
+sam_friday = ['''Friday:
+8-5''']
+
+
+
