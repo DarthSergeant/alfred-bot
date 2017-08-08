09f7cf99a36626cbad874639843f9ec33bc25277
DarthSergeant <darthsergeant@gmail.com>
2017-08-08 11:22:56 -0400

    Create hrl_processing.py

---
+++ bot_core/hrl/hrl_processing.py
@@ -0,0 +1,19 @@
+'''This imports a single function from information_processing to avoid extra
+import clutter from the other files saved in the information files.
+
+This file is then the one that is read by the main bot, keeping the main bot from
+touching anything in the hrl file except for this exported response.
+'''
+
+"""##########################
+# Imports
+##########################"""
+from hrl.information_processing import delegate_message
+
+"""##########################
+# HRL Responses
+##########################"""
+def export_hrl_response(text): 
+	return(delegate_message(text))
+
+
