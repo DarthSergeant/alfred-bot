3f4fc58ebc3c02cb060c1e1a03c22474a8f89888
DarthSergeant <darthsergeant@gmail.com>
2017-08-11 11:15:34 -0400

    Update ai.py

--- bot_core/ai.py
+++ bot_core/ai.py
@@ -8,10 +8,10 @@ from hrl.hrl_processing import export_hrl_response
 ##########################"""
 #create a function to determine which database to read from
 def post_response(text):
-        if "cat" in text:
-                return("cat")
-                   
-        #return(export_hrl_response(text))
+        #if "cat" in text:
+         #       return("cat")
+                  
+        return(export_hrl_response(text))
 
 #def find_database(text):
 #	if any(word in text for word in )
