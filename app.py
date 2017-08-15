96d9f7a63f9449eaa38aca9ae1ae27592cd72425
DarthSergeant <darthsergeant@gmail.com>
2017-08-15 11:20:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from flask import Flask, request
 
-from bot_core.ai import post_response
+#from bot_core.ai import post_response
 from outer.cat2 import post_response
 
 app = Flask(__name__)
